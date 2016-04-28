b4a39dd7c539459dec971228bdab2217a20cb719
Jung Hyun Kim <corosk@gmail.com>
2016-04-28 18:27:24 +0900

    중력 연습

--- study/gravity/js/app.js
+++ study/gravity/js/app.js
@@ -26,6 +26,7 @@ var Ball = (function () {
         this.speedX = 0;
         this.speedY = 0;
         this.gravitySpeed = 0; //중력 가속도
+        this.windSpeed = 0;
         this.bounce = 0.6; //탄성
     }
     return Ball;
@@ -33,6 +34,7 @@ var Ball = (function () {
 var Gravity = (function () {
     function Gravity() {
         this.gravity = 0.9; //중력
+        this.wind = 0; //바람
     }
     Gravity.prototype.accelerate = function () {
         this.gravity = -0.05;
@@ -69,15 +71,27 @@ var Display = (function (_super) {
     };
     Display.prototype.update = function () {
         this.obj.gravitySpeed += this.gravity;
-        this.obj.x += this.obj.speedX;
+        this.obj.windSpeed += this.wind;
+        this.obj.x += this.obj.speedX + this.obj.windSpeed;
         this.obj.y += this.obj.speedY + this.obj.gravitySpeed;
+        this.sideHit();
         this.bottomHit();
-        this.obj.rotate += Math.PI / 180;
+        this.obj.rotate += (Math.PI / 180) * this.obj.windSpeed;
     };
     Display.prototype.bottomHit = function () {
         if (this.obj.y >= canvas.height - this.obj.radius) {
             this.obj.y = canvas.height - this.obj.radius;
             this.obj.gravitySpeed = -(this.obj.gravitySpeed * this.obj.bounce); //바닥에 도착했을 때 현재 중력 가속도를 반대 방향으로 탄성만큼 곺한 값으로 바꿔주고 다시 렌더링에서 값을 양의 숫자쪽으로 증가시킨다.
+        }
+    };
+    Display.prototype.sideHit = function () {
+        if (this.obj.x >= canvas.width - this.obj.radius) {
+            this.obj.x = canvas.width - this.obj.radius;
+            this.obj.windSpeed = -(this.obj.windSpeed * this.obj.bounce);
+        }
+        else if (this.obj.x <= this.obj.radius) {
+            this.obj.x = this.obj.radius;
+            this.obj.windSpeed = -(this.obj.windSpeed * this.obj.bounce);
         }
     };
     Display.prototype.clear = function () {
@@ -92,8 +106,32 @@ var Display = (function (_super) {
 }(Gravity));
 var obj = new Ball({});
 var d = new Display(obj);
-canvas.addEventListener("mousedown", function () { return d.gravity = -0.9; });
-canvas.addEventListener("mouseup", function () { return d.gravity = 0.9; });
-canvas.addEventListener("touchstart", function () { return d.gravity = -0.9; });
-canvas.addEventListener("touchend", function () { return d.gravity = 0.9; });
+var Controller = (function () {
+    var w = document.getElementById("wind");
+    var wVal = document.getElementById("wind_val");
+    var wBtn = document.getElementById("wind_btn");
+    var wn = 0;
+    w.addEventListener("input", function () {
+        wn = parseFloat((w.value * 0.1).toFixed(1));
+        wVal.innerHTML = wn;
+    });
+    wBtn.addEventListener("click", function () {
+        d.wind = wn;
+    });
+    var g = document.getElementById("gravity");
+    var gVal = document.getElementById("gravity_val");
+    var gBtn = document.getElementById("gravity_btn");
+    var gn = 0;
+    g.addEventListener("input", function () {
+        gn = parseFloat((g.value * 0.1).toFixed(1));
+        gVal.innerHTML = gn;
+    });
+    gBtn.addEventListener("click", function () {
+        d.gravity = gn;
+    });
+    canvas.addEventListener("mousedown", function () { return d.gravity = -gn; });
+    canvas.addEventListener("mouseup", function () { return d.gravity = gn; });
+    canvas.addEventListener("touchstart", function () { return d.gravity = -gn; });
+    canvas.addEventListener("touchend", function () { return d.gravity = gn; });
+})();
 //# sourceMappingURL=app.js.map
--- study/gravity/js/app.ts
+++ study/gravity/js/app.ts
@@ -24,6 +24,7 @@ class Ball {
     speedX: number;
     speedY: number;
     gravitySpeed: number;
+    windSpeed: number;
     bounce: number;
     rotate: number;
     constructor(options: any){
@@ -37,14 +38,17 @@ class Ball {
         this.speedX = 0;
         this.speedY = 0;
         this.gravitySpeed = 0; //중력 가속도
+        this.windSpeed = 0;
         this.bounce = 0.6; //탄성
     }
 }
 
 class Gravity {
     gravity: number;
+    wind: number;
     constructor(){
         this.gravity = 0.9; //중력
+        this.wind = 0; //바람
     }
     accelerate(){
         this.gravity = -0.05;
@@ -81,15 +85,26 @@ class Display extends Gravity {
     }
     update(){
         this.obj.gravitySpeed += this.gravity;
-        this.obj.x += this.obj.speedX;
+        this.obj.windSpeed += this.wind;
+        this.obj.x += this.obj.speedX + this.obj.windSpeed;
         this.obj.y += this.obj.speedY + this.obj.gravitySpeed;
+        this.sideHit();
         this.bottomHit();
-        this.obj.rotate += Math.PI / 180;
+        this.obj.rotate += (Math.PI / 180)*this.obj.windSpeed;
     }
     bottomHit(){
         if(this.obj.y >= canvas.height - this.obj.radius){
             this.obj.y = canvas.height - this.obj.radius;
             this.obj.gravitySpeed = -(this.obj.gravitySpeed * this.obj.bounce);//바닥에 도착했을 때 현재 중력 가속도를 반대 방향으로 탄성만큼 곺한 값으로 바꿔주고 다시 렌더링에서 값을 양의 숫자쪽으로 증가시킨다.
+        }
+    }
+    sideHit(){
+        if(this.obj.x >= canvas.width - this.obj.radius){
+            this.obj.x = canvas.width - this.obj.radius;
+            this.obj.windSpeed = -(this.obj.windSpeed * this.obj.bounce);
+        }else if(this.obj.x <= this.obj.radius){
+            this.obj.x = this.obj.radius;
+            this.obj.windSpeed = -(this.obj.windSpeed * this.obj.bounce);
         }
     }
     clear(){
@@ -104,8 +119,35 @@ class Display extends Gravity {
 let obj = new Ball({});
 let d = new Display(obj);
 
-canvas.addEventListener("mousedown", ()=> d.gravity = -0.9 );
-canvas.addEventListener("mouseup", ()=> d.gravity = 0.9 );
+const Controller = (() =>{
+    const w:any = document.getElementById("wind");
+    const wVal:any = document.getElementById("wind_val");
+    const wBtn:any = document.getElementById("wind_btn");
+    let wn = 0;
+    w.addEventListener("input", ()=>{
+        wn = parseFloat( (w.value*0.1).toFixed(1) );
+        wVal.innerHTML = wn
+    });
+    wBtn.addEventListener("click", ()=>{
+        d.wind = wn;
+    });
 
-canvas.addEventListener("touchstart", ()=> d.gravity = -0.9 );
-canvas.addEventListener("touchend", ()=> d.gravity = 0.9 );
+    const g:any = document.getElementById("gravity");
+    const gVal:any = document.getElementById("gravity_val");
+    const gBtn:any = document.getElementById("gravity_btn");
+    let gn = 0;
+    g.addEventListener("input", ()=>{
+        gn = parseFloat( (g.value*0.1).toFixed(1) );
+        gVal.innerHTML = gn
+    });
+    gBtn.addEventListener("click", ()=>{
+        d.gravity = gn;
+    });
+
+    canvas.addEventListener("mousedown", ()=> d.gravity = -gn );
+    canvas.addEventListener("mouseup", ()=> d.gravity = gn );
+
+    canvas.addEventListener("touchstart", ()=> d.gravity = -gn );
+    canvas.addEventListener("touchend", ()=> d.gravity = gn );
+})();
+
